755ef71e77608499287569e51591b5ff21e33ce3
Nishant Mittal <mittalnishant14@outlook.com>
2020-11-28 20:15:19 +0530

    feat(dev-workflow): add husky pre commit hooks (#4)
    * add husky pre commit hook
    
    * format all files

--- scripts/package-lock-check.js
+++ scripts/package-lock-check.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-fs.readFile('package-lock.json', (err, data) => {
-    if (err) {
-        return;
-    }
-    throw new Error("package-lock.json found!");
-});
+const fs = require("fs")
+fs.readFile("package-lock.json", (err, data) => {
+  if (err) {
+    return
+  }
+  throw new Error("package-lock.json found!")
+})
