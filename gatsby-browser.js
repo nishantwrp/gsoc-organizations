ee5d5f07a4b3d1d9f24abadf62db0aedf53e182c
Nishant Mittal <mittalnishant14@outlook.com>
2020-12-22 21:23:08 +0530

    fix: hide main div until the page has finished loading (#33)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,5 +9,6 @@
 export const onInitialClientRender = () => {
   setTimeout(function () {
     document.getElementById("___loader").style.display = "none"
+    document.getElementById("___gatsby").style.display = "block"
   }, 1000)
 }
