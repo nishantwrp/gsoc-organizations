d28dafac156fe7911724f1bdb151a3e40277ea81
singlamohit1 <51117355+singlamohit1@users.noreply.github.com>
2020-12-23 12:51:01 +0530

    feat: add search functionality in filters (#34)
    * search in sidebar
    
    * done changes in search
    
    * Apply suggestions from code review
    
    Co-authored-by: Nishant Mittal <mittalnishant14@outlook.com>

--- src/components/filter-modal.jsx
+++ src/components/filter-modal.jsx
@@ -1,13 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Fuse from "fuse.js"
 
 import "./filter-modal.css"
 
 import { Modal, Button, Input, Grid, Checkbox } from "semantic-ui-react"
 
-const FilterModal = ({ trigger, name, optionsState, updateAllFilters }) => {
+const FilterModal = ({
+  trigger,
+  name,
+  optionsState,
+  updateAllFilters,
+  searchState,
+}) => {
   const [open, setOpen] = React.useState(false)
   let [allOptions, setOptions] = optionsState
+  const { searchQuery, setSearchQuery } = searchState
 
   const toggleChecked = index => {
     return () => {
@@ -18,14 +26,40 @@ const FilterModal = ({ trigger, name, optionsState, updateAllFilters }) => {
     }
   }
 
+  const getFuseSearch = allOptions => {
+    const options = {
+      threshold: 0.3,
+      keys: ["name"],
+    }
+
+    return new Fuse(allOptions, options)
+  }
+
+  const handleChange = e => {
+    setSearchQuery(e.target.value)
+  }
+
+  const getfilteredCheckboxes = (allOptions, searchQuery) => {
+    let filteredOrganizations = allOptions
+
+    if (searchQuery !== "") {
+      const fuse = getFuseSearch(allOptions)
+      filteredOrganizations = fuse.search(searchQuery).map(res => res.item)
+    }
+
+    return filteredOrganizations
+  }
+
+  let filteredCheckboxes = getfilteredCheckboxes(allOptions, searchQuery)
+
   let filterCheckboxes = []
-  for (let i = 0; i < allOptions.length; i++) {
+  for (let i = 0; i < filteredCheckboxes.length; i++) {
     filterCheckboxes.push(
       <Grid.Column>
         <Checkbox
-          checked={allOptions[i].selected}
-          label={allOptions[i].name}
-          value={allOptions[i].selected}
+          checked={filteredCheckboxes[i].selected}
+          label={filteredCheckboxes[i].name}
+          value={filteredCheckboxes[i].selected}
           onChange={toggleChecked(i)}
         />
       </Grid.Column>
@@ -46,7 +80,8 @@ const FilterModal = ({ trigger, name, optionsState, updateAllFilters }) => {
             size="small"
             icon="search"
             className="filter-modal-content-search"
-            placeholder="Search technologies"
+            onChange={handleChange.bind(this)}
+            placeholder={`Search ${name}`}
           />
           <div className="filter-modal-content-filters">
             <Grid stackable columns={3}>
--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
+import Fuse from "fuse.js"
 
 import "./filter.css"
 
@@ -7,7 +8,9 @@ import FilterModal from "./filter-modal"
 import { Checkbox, Input, Divider } from "semantic-ui-react"
 
 const Filter = ({ name, optionsState, showDivider, updateAllFilters }) => {
+  const [hidden, setHidden] = useState(true)
   let [allOptions, setOptions] = optionsState
+  const [searchQuery, setSearchQuery] = React.useState("")
 
   const toggleChecked = index => {
     return () => {
@@ -18,15 +21,41 @@ const Filter = ({ name, optionsState, showDivider, updateAllFilters }) => {
     }
   }
 
+  const getFuseSearch = allOptions => {
+    const options = {
+      threshold: 0.3,
+      keys: ["name"],
+    }
+
+    return new Fuse(allOptions, options)
+  }
+
+  const handleChange = e => {
+    setSearchQuery(e.target.value)
+  }
+
+  const getfilteredCheckboxes = (allOptions, searchQuery) => {
+    let filteredOrganizations = allOptions
+
+    if (searchQuery !== "") {
+      const fuse = getFuseSearch(allOptions)
+      filteredOrganizations = fuse.search(searchQuery).map(res => res.item)
+    }
+
+    return filteredOrganizations
+  }
+
+  let filteredCheckboxes = getfilteredCheckboxes(allOptions, searchQuery)
+
   let filterCheckboxes = []
-  for (let i = 0; i < allOptions.length && i < 5; i++) {
+  for (let i = 0; i < filteredCheckboxes.length && i < 5; i++) {
     filterCheckboxes.push(
       <tr>
         <td>
           <Checkbox
-            checked={allOptions[i].selected}
-            label={allOptions[i].name}
-            value={allOptions[i].selected}
+            checked={filteredCheckboxes[i].selected}
+            label={filteredCheckboxes[i].name}
+            value={filteredCheckboxes[i].selected}
             onChange={toggleChecked(i)}
           />
         </td>
@@ -42,7 +71,12 @@ const Filter = ({ name, optionsState, showDivider, updateAllFilters }) => {
         <u>{name}</u>
       </div>
       <div className="filter-search">
-        <Input size="mini" icon="search" placeholder={`Search ${name}`} />
+        <Input
+          size="mini"
+          icon="search"
+          placeholder={`Search ${name}`}
+          onChange={handleChange.bind(this)}
+        />
       </div>
       <div className="filter-boxes">
         <center>
@@ -57,6 +91,10 @@ const Filter = ({ name, optionsState, showDivider, updateAllFilters }) => {
           optionsState={optionsState}
           updateAllFilters={updateAllFilters}
           trigger={<div className="filter-view-more">View all</div>}
+          searchState={{
+            searchQuery: searchQuery,
+            setSearchQuery: setSearchQuery,
+          }}
         />
       </div>
       <div style={showDivider ? {} : { display: "none" }}>
