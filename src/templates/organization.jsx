71dcc1e99b59d6921207543e7c0a76a87ac9eeff
Nishant Mittal <mittalnishant14@outlook.com>
2021-01-27 04:36:32 +0530

    fix: add timeout to adsense script

--- src/templates/organization.jsx
+++ src/templates/organization.jsx
@@ -67,6 +67,12 @@ const OrganizationPage = ({ pageContext: { organization }, data }) => {
     },
   ]
 
+  React.useEffect(() => {
+    setTimeout(() => {
+      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+    }, 2000)
+  }, [])
+
   return (
     <Layout homePage={false}>
       <SEO title={organization.name} meta={meta} />
@@ -77,24 +83,15 @@ const OrganizationPage = ({ pageContext: { organization }, data }) => {
         </Grid.Column>
         <Grid.Column>
           <ProjectsGraph data={organization.years} />
-          <div>
-            <script
-              async
-              src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-            ></script>
+          <div className="organization-graph-ad-container">
             <ins
-              class="adsbygoogle"
+              className="adsbygoogle"
               style={{ display: "block" }}
               data-ad-client="ca-pub-9769516184087442"
               data-ad-slot="9135360049"
               data-ad-format="auto"
               data-full-width-responsive="true"
             ></ins>
-            <script
-              dangerouslySetInnerHTML={{
-                __html: "(adsbygoogle = window.adsbygoogle || []).push({});",
-              }}
-            ></script>
           </div>
         </Grid.Column>
       </Grid>
