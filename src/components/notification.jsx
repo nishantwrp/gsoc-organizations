aa3cecfb202f89023f5d9abce3d565578aab6cd9
singlamohit1 <51117355+singlamohit1@users.noreply.github.com>
2021-03-10 08:46:55 +0530

    feat: add a notice for gsoc 2021 orgs (#49)

---
+++ src/components/notification.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { Message } from "semantic-ui-react"
+
+const Notification = () => {
+  const style = {
+    width: "100%",
+    margin: "1rem",
+  }
+  return (
+    <Message positive style={style}>
+      <Message.Header>
+        Official ogranizations for GSoC 2021 have been announced. You can check
+        them{" "}
+        <a
+          href="https://summerofcode.withgoogle.com/organizations/"
+          target="_blank"
+        >
+          <u>here</u>
+        </a>
+        .
+      </Message.Header>
+    </Message>
+  )
+}
+
+export default Notification
