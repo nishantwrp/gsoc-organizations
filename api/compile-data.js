b277e4efda972fca7647d5e6b84f3e289bb5ad0a
Nishant Mittal <mittalnishant14@outlook.com>
2021-09-29 22:21:50 +0530

    fix: improve data compilation script

--- api/compile-data.js
+++ api/compile-data.js
@@ -12,130 +12,143 @@ const getDataPath = year => {
   return `api/data/${year}.json`
 }
 
-let gsocOrganizations = []
+class DisjointSet {
+  _parents = {}
 
-const getOrCreateOrganization = orgJson => {
-  for (const [index, org] of Object.entries(gsocOrganizations)) {
-    if (org.name === orgJson.name) {
-      return index
+  _getHashFromObject(obj) {
+    return JSON.stringify(obj)
+  }
+
+  _getObjectFromHash(hash) {
+    return JSON.parse(hash)
+  }
+
+  _findSet(hash) {
+    if (this._parents[hash] === hash) {
+      return hash
     }
 
-    const originalUrl = new URL(org.url)
-    const incomingUrl = new URL(orgJson.url)
+    this._parents[hash] = this._findSet(this._parents[hash])
+    return this._parents[hash]
+  }
 
-    if (
-      originalUrl.host === incomingUrl.host &&
-      originalUrl.pathname === incomingUrl.pathname
-    ) {
-      return index
+  add(obj) {
+    const hash = this._getHashFromObject(obj)
+    if (!(hash in this._parents)) {
+      this._parents[hash] = hash
     }
   }
 
-  gsocOrganizations.push({
-    name: "",
-    url: "",
-    image_url: "",
-    image_background_color: "",
-    description: "",
-    category: "",
-    topics: [],
-    technologies: [],
-    years: {},
-  })
-  return gsocOrganizations.length - 1
-}
+  union(obj1, obj2) {
+    let hash1 = this._getHashFromObject(obj1)
+    let hash2 = this._getHashFromObject(obj2)
 
-const getLatestGsocYear = index => {
-  const years = Object.keys(gsocOrganizations[index].years).sort()
+    hash1 = this._findSet(hash1)
+    hash2 = this._findSet(hash2)
 
-  if (years.length) {
-    return Number.parseInt(years[years.length - 1])
+    if (hash1 !== hash2) {
+      this._parents[hash1] = hash2
+    }
   }
 
-  return 0
-}
+  getAllElements() {
+    const allHashes = Object.keys(this._parents)
+    return allHashes.map(hash => this._getObjectFromHash(hash))
+  }
 
-const updateBasicOrgInfo = (
-  index,
-  year,
-  name,
-  imageUrl,
-  imageBackgroundColor,
-  description,
-  url,
-  irc_channel,
-  contact_email,
-  mailing_list,
-  category
-) => {
-  if (getLatestGsocYear(index) < year) {
-    gsocOrganizations[index].name = name
-    gsocOrganizations[index].image_url = imageUrl
-    gsocOrganizations[index].image_background_color = imageBackgroundColor
-    gsocOrganizations[index].description = description
-    gsocOrganizations[index].url = url
-    gsocOrganizations[index].irc_channel = irc_channel
-    gsocOrganizations[index].contact_email = contact_email
-    gsocOrganizations[index].mailing_list = mailing_list
-    gsocOrganizations[index].category = category
+  extract() {
+    const allParentHashes = []
+
+    Object.keys(this._parents).forEach(hash => {
+      hash = this._findSet(hash)
+      if (!allParentHashes.includes(hash)) {
+        allParentHashes.push(hash)
+      }
+    })
+
+    const allSets = []
+    for (const parentHash of allParentHashes) {
+      const elementsInThisSet = []
+
+      Object.keys(this._parents).forEach(hash => {
+        if (this._findSet(hash) === parentHash) {
+          elementsInThisSet.push(this._getObjectFromHash(hash))
+        }
+      })
+
+      allSets.push(elementsInThisSet)
+    }
+    return allSets
   }
 }
 
-const updateYears = (index, year, projects_url, num_projects) => {
-  gsocOrganizations[index].years[year] = {
+const isMergePossible = (org1, org2) => {
+  const normalizeUrlHost = host => {
+    return host.startsWith("www.") ? host.slice(4) : host
+  }
+
+  // Case insensitive comparison
+  if (org1.name.toUpperCase() === org2.name.toUpperCase()) {
+    return true
+  }
+
+  const url1 = new URL(org1.url)
+  const url2 = new URL(org2.url)
+
+  if (
+    normalizeUrlHost(url1.host) === normalizeUrlHost(url2.host) &&
+    url1.pathname === url2.pathname
+  ) {
+    return true
+  }
+
+  return false
+}
+
+const updateYears = (combinedJson, year, projects_url, num_projects) => {
+  combinedJson.years[year] = {
     projects_url: projects_url,
     num_projects: num_projects,
   }
 }
 
-const updateTopics = (index, year, topics) => {
+const updateTopics = (combinedJson, topics) => {
   for (const topic of topics) {
-    if (!gsocOrganizations[index].topics.includes(topic)) {
-      gsocOrganizations[index].topics.push(topic)
+    if (!combinedJson.topics.includes(topic)) {
+      combinedJson.topics.push(topic)
     }
   }
 }
 
-const updateTechnologies = (index, year, technologies) => {
+const updateTechnologies = (combinedJson, technologies) => {
   for (const technology of technologies) {
-    if (!gsocOrganizations[index].technologies.includes(technology)) {
-      gsocOrganizations[index].technologies.push(technology)
+    if (!combinedJson.technologies.includes(technology)) {
+      combinedJson.technologies.push(technology)
     }
   }
 }
 
-const updateOrg = (index, year, orgJson) => {
-  const {
-    name,
-    image_url,
-    image_background_color,
-    description,
-    url,
-    irc_channel,
-    contact_email,
-    mailing_list,
-    category,
-    projects_url,
-    topics,
-    technologies,
-    num_projects,
-  } = orgJson
-  updateBasicOrgInfo(
-    index,
-    year,
-    name,
-    image_url,
-    image_background_color,
-    description,
-    url,
-    irc_channel,
-    contact_email,
-    mailing_list,
-    category
-  )
-  updateYears(index, year, projects_url, num_projects)
-  updateTopics(index, year, topics)
-  updateTechnologies(index, year, technologies)
+const updateOrg = (combinedJson, orgJson) => {
+  const { projects_url, topics, technologies, num_projects, year } = orgJson
+
+  const basic_properties = [
+    "name",
+    "image_url",
+    "image_background_color",
+    "description",
+    "url",
+    "irc_channel",
+    "contact_email",
+    "mailing_list",
+    "category",
+  ]
+  for (const prop of basic_properties) {
+    combinedJson[prop] = orgJson[prop] || combinedJson[prop]
+  }
+
+  updateYears(combinedJson, year, projects_url, num_projects)
+  updateTopics(combinedJson, topics)
+  updateTechnologies(combinedJson, technologies)
 }
 
 const applyFilters = orgJson => {
@@ -165,21 +178,57 @@ const applyFilters = orgJson => {
   orgJson.technologies = technologies
 }
 
-const processData = data => {
-  for (const orgJson of data.organizations) {
-    applyFilters(orgJson)
-    const index = getOrCreateOrganization(orgJson)
-    updateOrg(index, Number.parseInt(data.year), orgJson)
+const getCombinedOrgJson = orgList => {
+  const combinedJson = {
+    name: "",
+    url: "",
+    image_url: "",
+    image_background_color: "",
+    description: "",
+    category: "",
+    topics: [],
+    technologies: [],
+    years: {},
+    irc_channel: "",
+    contact_email: "",
+    mailing_list: "",
   }
+
+  orgList = orgList.sort((a, b) => {
+    return a.year > b.year ? 1 : -1
+  })
+
+  for (const orgJson of orgList) {
+    updateOrg(combinedJson, orgJson)
+  }
+
+  return combinedJson
 }
 
 const compileData = () => {
-  gsocOrganizations = []
+  const organizationSet = new DisjointSet()
 
   for (const year of YEARS) {
-    const rawData = fs.readFileSync(getDataPath(year))
-    processData(JSON.parse(rawData))
+    const data = JSON.parse(fs.readFileSync(getDataPath(year)))
+
+    for (const currentOrg of data.organizations) {
+      applyFilters(currentOrg)
+      currentOrg.year = Number.parseInt(data.year)
+
+      organizationSet.add(currentOrg)
+      for (const otherOrg of organizationSet.getAllElements()) {
+        if (isMergePossible(currentOrg, otherOrg)) {
+          organizationSet.union(currentOrg, otherOrg)
+        }
+      }
+    }
   }
+
+  const distinctOrganizations = organizationSet.extract()
+  const gsocOrganizations = []
+  distinctOrganizations.forEach(orgList => {
+    gsocOrganizations.push(getCombinedOrgJson(orgList))
+  })
 
   const sortedOrganizations = gsocOrganizations.sort((a, b) => {
     if (a.name === b.name) {
