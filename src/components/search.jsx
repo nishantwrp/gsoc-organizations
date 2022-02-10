ac811afd5d0e990b90fdf8a781c8ef1814ca593f
Nishant Mittal <mittalnishant14@outlook.com>
2022-02-11 01:59:17 +0530

    feat: debounced search

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useMemo, useState, useEffect } from "react"
+import { debounce } from "debounce"
 
 import "./search.css"
 
@@ -9,15 +10,32 @@ import { getSearch, setSearch } from "../store/search"
 const Search = () => {
   const search = useAppSelector(getSearch)
   const dispatch = useAppDispatch()
+  const [searchText, setSearchText] = useState(search)
 
-  const handleChange = e => {
-    dispatch(setSearch(e.target.value))
+  const dispatchSetSearch = value => {
+    dispatch(setSearch(value))
+  }
+
+  const debouncedDispatchSetSearch = useMemo(
+    () => debounce(dispatchSetSearch, 200),
+    []
+  )
+
+  useEffect(() => {
+    return () => {
+      debouncedDispatchSetSearch.cancel()
+    }
+  }, [])
+
+  const handleChange = ({ target: { value } }) => {
+    setSearchText(value)
+    debouncedDispatchSetSearch(value)
   }
 
   return (
     <div className="search-search">
       <Input icon placeholder="Search">
-        <input value={search} onChange={handleChange.bind(this)} />
+        <input value={searchText} onChange={handleChange.bind(this)} />
         <Icon name="search" />
       </Input>
     </div>
