da44e13b725016e99712ca361f35d424c0821721
singlamohit1 <singlamohit98@gmail.com>
2023-02-05 21:45:54 +0530

    made feature to save orgs

---
+++ src/pages/stars.jsx
@@ -0,0 +1,165 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { useBreakpoint } from "gatsby-plugin-breakpoints"
+import { Button } from "semantic-ui-react"
+import "./index.css"
+import { getBookmarkedOrgsCount } from "../store/bookmark"
+import Layout from "../layouts/layout"
+import OrgCard from "../components/org-card"
+import { Grid } from "semantic-ui-react"
+import { Link } from "gatsby"
+import { clearCount, removeAll } from "../store/bookmark"
+import { useAppDispatch, useAppSelector } from "../store"
+
+const getOrganizations = data => {
+  return data.allOrganization.edges.map(orgNode => {
+    let org = orgNode.node
+    for (const yearKey of Object.keys(org.years)) {
+      if (yearKey[0] === "_") {
+        if (org.years[yearKey] !== null) {
+          let year = yearKey.slice(1)
+          org.years[year] = org.years[yearKey]
+        }
+        delete org.years[yearKey]
+      }
+    }
+    return org
+  })
+}
+
+const getFilteredOrganizations = data => {
+  const organizations = getOrganizations(data)
+  let filteredOrganizations = []
+  let starredOrgs
+
+  if (typeof window !== "undefined") {
+    starredOrgs = JSON.parse(localStorage.getItem("gsoc_orgs"))
+  }
+
+  if (starredOrgs != null) {
+    for (const org of organizations) {
+      if (starredOrgs.indexOf(org.name) > -1) {
+        filteredOrganizations.push(org)
+      }
+    }
+  }
+
+  return filteredOrganizations
+}
+
+const StarsPage = ({ data }) => {
+  const dispatch = useAppDispatch()
+  let filteredOrganizations = getFilteredOrganizations(data)
+  function clearStars() {
+    removeAll()
+    dispatch(clearCount())
+  }
+
+  const cards = []
+  for (const organization of filteredOrganizations) {
+    cards.push(
+      <Grid.Column>
+        <OrgCard data={organization} />
+      </Grid.Column>
+    )
+  }
+  const starCount = useAppSelector(getBookmarkedOrgsCount)
+  const cardColumns = useBreakpoint().l ? 3 : 4
+  return (
+    <Layout>
+      <div style={{ marginTop: "1rem", textAlign: "center" }}>
+        <a className="ui yellow label">{starCount} stars</a>
+      </div>
+
+      <div
+        style={{ marginTop: "1rem", textAlign: "center", marginBottom: "1rem" }}
+      >
+        <Link to={`/`}>
+          <div className="sidebar-content-clear-filters">
+            <Button
+              size="tiny"
+              basic
+              color="orange"
+              onClick={() => clearStars()}
+            >
+              Clear bookmarks
+            </Button>
+          </div>
+        </Link>
+      </div>
+
+      <div></div>
+
+      <Grid className="index-org-cards-grid" stackable columns={cardColumns}>
+        {cards}
+      </Grid>
+      <div style={{ padding: "1rem" }}>
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-9769516184087442"
+          data-ad-slot="5525920548"
+          data-ad-format="auto"
+          data-full-width-responsive="false"
+        ></ins>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        siteUrl
+      }
+    }
+    allOrganization {
+      edges {
+        node {
+          category
+          description
+          name
+          technologies
+          image_url
+          image_background_color
+          topics
+          url
+          years {
+            _2016 {
+              num_projects
+              projects_url
+            }
+            _2017 {
+              num_projects
+              projects_url
+            }
+            _2018 {
+              num_projects
+              projects_url
+            }
+            _2019 {
+              num_projects
+              projects_url
+            }
+            _2020 {
+              num_projects
+              projects_url
+            }
+            _2021 {
+              num_projects
+              projects_url
+            }
+            _2022 {
+              num_projects
+              projects_url
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default StarsPage
