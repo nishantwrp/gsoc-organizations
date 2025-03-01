9dee2ca8516bb36a933dbf41b8c6880e6cd3adc3
Nishant Mittal <mittalnishant14@outlook.com>
2025-03-01 13:52:20 +0530

    other routine updates

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -97,7 +97,7 @@ const Sidebar = ({ config, showFilters }) => {
           <div className="sidebar-content-filters">
             <Filter
               name="shortcuts"
-              choices={[{ name: "First-time organizations", frequency: 15 }]}
+              choices={[{ name: "First-time organizations", frequency: 14 }]}
               sortBy="frequency"
             />
             <Filter name="years" choices={years} sortBy="name" order="desc" />
