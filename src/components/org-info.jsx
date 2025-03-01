ecdf45a9ce2adc22b6b465d66f53aaa4e2ce24cc
Nishant Mittal <mittalnishant14@outlook.com>
2025-03-01 13:55:02 +0530

    fix: updates to 2024 data, improve 2025 data quality, misc improvements (#166)
    * add data for 2025, update for 2024
    
    * other routine updates

--- src/components/org-info.jsx
+++ src/components/org-info.jsx
@@ -36,7 +36,7 @@ const OrgInfo = ({ data }) => {
     return <span className="org-info-topic">{topic}</span>
   })
 
-  const isParticipatingIn2024 = "2024" in data.years
+  const isParticipatingIn2025 = "2025" in data.years
 
   return (
     <div className="org-info-container">
@@ -53,16 +53,16 @@ const OrgInfo = ({ data }) => {
           }}
         ></div>
       </div>
-      {/* {isParticipatingIn2024 && (
+      {isParticipatingIn2025 && (
         <Message color="orange" style={{ margin: "1%" }}>
           <Message.Header>
             {data.name} is participating in{" "}
             <OutboundLink
-              href={data.years["2024"].projects_url}
+              href={data.years["2025"].projects_url}
               rel="noreferrer"
               target="_blank"
             >
-              <u>GSoC 2024</u>
+              <u>GSoC 2025</u>
             </OutboundLink>
             . View the{" "}
             <OutboundLink
@@ -87,7 +87,7 @@ const OrgInfo = ({ data }) => {
             for this organization.
           </Message.Header>
         </Message>
-      )} */}
+      )}
       <div className="org-info-site-container">
         <OutboundLink href={data.url} rel="noreferrer" target="_blank">
           <Button icon labelPosition="left" color="orange">
